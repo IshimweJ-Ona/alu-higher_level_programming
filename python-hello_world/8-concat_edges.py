ac36e3f5b85bbaf09fc5c2cced6d86fb38dc222c
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2025-03-03 02:18:57 -0800

    FIxed programs

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = str[39:66]
-str2 = str[:6]
-print(f"{sr1} {str2}")
+print(f"{str[39:66]} {str[:6]}")
