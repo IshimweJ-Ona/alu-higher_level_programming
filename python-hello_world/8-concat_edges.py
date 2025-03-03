8926477b67c31bd38da52c5d06822b01b6a16055
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2025-03-03 01:58:45 -0800

    Progam is not easy

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = str[39:66]
+str2 = str[:6]
+print(f"{sr1} {str2}")
